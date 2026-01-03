1d657d6459dbd351308d185d24bb516faed8baa8
vinithp03 <vinithpoojary2001@gmail.com>
2026-01-03 20:20:06 +0530

    Keep only deployment configs (docker, nginx)

--- backend/cart/src/main/java/com/vinith/cart/RepositoryLayer/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vinith.cart.RepositoryLayer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.vinith.cart.EntityLayer.CartItem;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
--- backend/catalog/src/main/java/com/vinith/catalog/RepositoryLayer/RatingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vinith.catalog.RepositoryLayer;
-
-import com.vinith.catalog.EntityLayer.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Long> {
-    
-}
